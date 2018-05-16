967ace78a6b656e99bb0b7d15f9c2c1f732ec56d
Jose Villalta <villalta.jose@gmail.com>
2018-05-16 08:24:28 -0700

    linked list

---
+++ linkedLists/linked_list.go
@@ -0,0 +1,18 @@
+package linkedLists
+
+type ListNode struct {
+	data int
+	next *ListNode
+}
+
+type LinkedList struct {
+	head *ListNode
+}
+
+func (l * LinkedList) Insert(val int) {
+
+}
+
+func (l * LinkedList) Remove(val int) {
+
+}
