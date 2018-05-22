5ffd1ba4acffff23f97703e689297db1dc4b3aef
villalta.jose@gmail.com <villalta.jose@gmail.com>
2018-05-22 08:09:06 -0700

    Learning go. Added a function with tests.

--- linkedLists/linked_list.go
+++ linkedLists/linked_list.go
@@ -1,18 +1,44 @@
-package linkedLists
+package linkedlists
 
+import "fmt"
+
+// ListNode contains the element of the linked list.
 type ListNode struct {
 	data int
 	next *ListNode
 }
 
+// LinkedList class to learn go.
 type LinkedList struct {
 	head *ListNode
+	back *ListNode
 }
 
-func (l * LinkedList) Insert(val int) {
+// Inserts a value at the end of the list
+func (l *LinkedList) Insert(val int) {
+	node := new(ListNode)
+	node.data = val
+
+	if l.head == nil {
+		l.head = node
+	}
+	if l.back == nil {
+		l.back = node
+	} else {
+		l.back.next = node
+		l.back = node
+	}
 
 }
 
-func (l * LinkedList) Remove(val int) {
+func (l *LinkedList) Remove(val int) {
 
+}
+
+func (l * LinkedList) PrintAllValues() {
+	nodePtr := l.head
+	for nodePtr != nil {
+		fmt.Println(nodePtr.data)
+		nodePtr = nodePtr.next
+	}
 }
--- linkedLists/linked_list_test.go
+++ linkedLists/linked_list_test.go
@@ -1,1 +1,16 @@
-package linkedLists
+package linkedlists
+
+import (
+	"testing"
+	"fmt"
+)
+
+func TestLinkedList_Insert(t *testing.T) {
+	myList := new(LinkedList)
+	myList.Insert(1)
+	fmt.Println("Inserted a value")
+	myList.Insert(2)
+	myList.Insert(3)
+	myList.PrintAllValues()
+
+}
